0c673df9d1013f2bfa0361cb5429b69f54e60c05
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-05-01 19:01:35 +0300

    Updates

---
+++ precache-manifest.3aff886f354c314be4794e40466c0742.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5702d81c4dbfb66317b1c5f0ca662e8",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "8ee71a39592d4c95a988",
+    "url": "/goit-react-hw-02/static/css/main.d204c58d.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/goit-react-hw-02/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "8ee71a39592d4c95a988",
+    "url": "/goit-react-hw-02/static/js/main.309a9339.chunk.js"
+  },
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  }
+]);
